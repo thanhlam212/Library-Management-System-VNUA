45412d4b292a1500a4bb9c1e9de66de7e4ce55fe
thanhLam <nguyenthanhlam02122003@gmail.com>
2023-10-10 10:06:36 +0700

    done 1.5/2  Librarians

--- Library Management System-VNUA/LibrariansForm.cs
+++ Library Management System-VNUA/LibrariansForm.cs
@@ -22,27 +22,65 @@ namespace Library_Management_System_VNUA
 
         private void LibrariansForm_Load(object sender, EventArgs e)
         {
-           
+            Populate();
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void Populate()
+        {
+            try
+            {
+                conn.Open();
+                string query = "SelectDataLibrarians";
+                SqlDataAdapter dap = new SqlDataAdapter(query, conn);
+                SqlCommandBuilder cmd = new SqlCommandBuilder(dap);
+                DataSet ds = new DataSet();
+                dap.Fill(ds);
+                StudentsDataTable.DataSource = ds.Tables[0];
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void AddStudentsBtn_Click(object sender, EventArgs e)
         {
-            if(LibID.Text == "" || LibName.Text == "" || LibPass.Text == "" || LibPhone.Text == "")
+            try
             {
-                MessageBox.Show("Missing Information");
-            }
-            else
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("InsertDataLibrarians",conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@LibID", LibID.Text.Trim());
-                cmd.Parameters.AddWithValue("@LibName", LibName.Text.Trim());
-                cmd.Parameters.AddWithValue("@LibPassword", LibPass.Text.Trim());
-                cmd.Parameters.AddWithValue("@LibPhone", LibPhone.Text.Trim());
+                if (LibID.Text == "" || LibName.Text == "" || LibPass.Text == "" || LibPhone.Text == "")
+                {
+                    MessageBox.Show("Missing Information");
+                }
+                else
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("InsertDataLibrarians", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@LibID", LibID.Text.Trim());
+                    cmd.Parameters.AddWithValue("@LibName", LibName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@LibPassword", LibPass.Text.Trim());
+                    cmd.Parameters.AddWithValue("@LibPhone", LibPhone.Text.Trim());
 
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Librarians Added Successfully.");
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Librarians Added Successfully.");
+                    Populate();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
                 conn.Close();
             }
         }
